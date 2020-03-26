0d145fb4f80d0fe8bdfbecdfea543d552888d63b
Rodrigo Araújo <rodrigo.rsa2005@gmail.com>
2020-03-25 23:14:41 -0300

    Primeiro Commit

---
+++ src/test/java/br/com/rodrigo/NativeaplicacaoResourceIT.java
@@ -0,0 +1,9 @@
+package br.com.rodrigo;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeaplicacaoResourceIT extends aplicacaoResourceTest {
+
+    // Execute the same tests but in native mode.
+}
